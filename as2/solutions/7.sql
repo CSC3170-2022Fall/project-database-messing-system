4b6a4f9a1216eaf448d6656a010067231525bb89
Aurora121090841 <102466995+Aurora121090841@users.noreply.github.com>
2022-12-24 01:13:03 +0800

    Update 7.sql

--- as2/solutions/7.sql
+++ as2/solutions/7.sql
@@ -3,5 +3,5 @@ create table out as
 select EMPLOYEE_ID, PHONE_NUMBER from employees
 where DEPARTMENT_ID not in
 select DEPARTMENT_ID from employees
-where DEPARTMENT_ID='20' and DEPARTMENT_ID !='100';
+where DEPARTMENT_ID!='20' and DEPARTMENT_ID !='100';
 store out out;
