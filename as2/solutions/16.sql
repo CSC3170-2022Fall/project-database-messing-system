1af004ee91920738c974b1756790ceef72b30f8e
Aurora121090841 <102466995+Aurora121090841@users.noreply.github.com>
2022-12-24 13:54:21 +0800

    Update 16.sql

--- as2/solutions/16.sql
+++ as2/solutions/16.sql
@@ -4,7 +4,9 @@ create table tep1 as
 select DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID from departments;
 create table tep2 as
 select EMPLOYEE_ID,FIRST_NAME from employees;
-create table out as
+create table preout as
 select DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID, EMPLOYEE_ID,FIRST_NAME from tep1, tep2
 where MANAGER_ID = EMPLOYEE_ID;
+create table out as 
+select DEPARTMENT_ID, DEPARTMENT_NAME,FIRST_NAME from preout;
 store out out;
